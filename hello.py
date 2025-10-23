38e52f3dd14d04e6acb0bcaa76054c164c6b1216
Виталий Иванищев <ivanishchevvm@mail.ru>
2025-10-23 13:03:35 +0300

    Незначительная работа с текстом в hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,17 @@
-print('Привет, Python!')
+def calculate_sum(a, b):
+ c = a + b
+ d = 10  # Неиспользуемая переменная
+ print(c)
+ print(e)  # Неопределенная переменная
+
+import math  # Неиспользуемый импорт
+
+x = 5
+y = 10
+z = x + y
+print(z)
+
+def another_function():
+   pass  # Неиспользуемая функция
+
+print('Hello, World!')
